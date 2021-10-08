916a1d33125664f43e2d663dae0446f096412fa0
System Administrator <root@Marcelos-Mac-Pro.local>
2021-10-08 16:58:06 -0300

    navigation bar

--- lib/app/app_widget.dart
+++ lib/app/app_widget.dart
@@ -6,13 +6,12 @@ class AppWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Ze Gotinha',
-      theme: ThemeData(
-        primaryColor: Colors.blue, 
-        backgroundColor: Colors.blue, 
+      theme: ThemeData( 
         colorScheme: ColorScheme.light(
           primary: Colors.blue,
           secondary: Colors.pink.shade400,
-
+          background: Colors.blue,
+          surface: Colors.white
       )),
     ).modular();
   }
--- lib/app/modules/home/home_page.dart
+++ lib/app/modules/home/home_page.dart
@@ -14,6 +14,65 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends ModularState<HomePage, HomeStore> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    double _height = MediaQuery.of(context).size.height; //screen height
+    double _width = MediaQuery.of(context).size.width; //screen width
+
+    _tabController(int option) {
+      switch (option) {
+        case 0: print(0); break;
+        case 1: print(1);break;
+        case 2: print(2);break;
+        case 3: print(3);break;
+      }
+
+    }
+
+    return Scaffold(
+      body: Container(
+        //* ******************************************************************** Backgroud
+        color: Theme.of(context).colorScheme.background,
+        height: _height,
+        width: _width,
+        child: Column(children: [
+          Container( //********************************************************* Navigation bar */
+            margin: const EdgeInsets.only(top: 40),
+            width: _width * .5,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+              color: Theme.of(context).colorScheme.surface,
+            ),
+            child: DefaultTabController(
+              length: 4,
+              child: TabBar(
+                labelColor: Theme.of(context).colorScheme.onSecondary,
+                indicator: BoxDecoration(
+                  color: Theme.of(context).colorScheme.secondary,
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                tabs: const [
+                  Tab(
+                    icon: Icon(Icons.home),
+                    text: "Zé Gotinha",
+                  ),
+                  Tab(
+                    icon: Icon(Icons.document_scanner_outlined),
+                    text: "Cartão de Vacina",
+                  ),
+                  Tab(
+                    icon: Icon(Icons.medical_services),
+                    text: "Consulta",
+                  ),
+                  Tab(
+                    icon: Icon(Icons.person),
+                    text: "Úsuario",
+                  ),
+                ],
+                onTap: (int op) => _tabController(op),
+              ),
+            ),
+          ),
+        ]),
+      ),
+    );
   }
 }
