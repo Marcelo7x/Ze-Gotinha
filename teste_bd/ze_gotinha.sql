5821c2229c2cf60a923b9c2d86afc73eb0c6ecc3
larissatonetto <50343825+larissatonetto@users.noreply.github.com>
2021-10-17 23:11:22 -0300

    Mais dados adicionados

--- teste_bd/ze_gotinha.sql
+++ teste_bd/ze_gotinha.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 16-Out-2021 às 14:48
+-- Tempo de geração: 18-Out-2021 às 04:10
 -- Versão do servidor: 10.4.18-MariaDB
 -- versão do PHP: 8.0.3
 
@@ -38,6 +38,8 @@ CREATE TABLE `cartao_vacina` (
 
 INSERT INTO `cartao_vacina` (`cpf`, `nome`) VALUES
 ('14921391491', 'Clara Rayssa Olivia Ribeiro'),
+('20446707120', 'Elza Mariah da Cunha'),
+('57726598638', 'Pedro Ramos'),
 ('58196919891', 'Diego Hugo Breno Moraes');
 
 -- --------------------------------------------------------
@@ -62,7 +64,11 @@ CREATE TABLE `consulta` (
 
 INSERT INTO `consulta` (`id_consulta`, `data`, `sintomas`, `pedidos_exames`, `receita`, `cpf`, `crm`) VALUES
 (1, '2021-10-07', 'Dor de cabeça', 'Exame de sangue', 'Dipirona 500mg', '14921391491', '56782-ES'),
-(2, '2021-10-14', 'Febre, tosse', 'Ressonância', 'Paracetamol 1000mg', '58196919891', '56782-ES');
+(2, '2021-10-14', 'Febre, tosse', 'Ressonância', 'Paracetamol 1000mg', '58196919891', '56782-ES'),
+(3, '2021-10-14', 'Dor de cabeça', 'Exame de sangue', 'Dipirona 500mg', '57726598638', '32474-GO'),
+(4, '2021-10-14', 'Febre, tosse', 'Exame de sangue', 'Resfenol', '20446707120', '32474-GO'),
+(5, '2021-10-04', 'Dor no peito, falta de ar', 'Eletrocardiograma', 'Dipirona 1000mg', '57726598638', '56782-ES'),
+(6, '2021-10-07', 'Dor nas articulações', 'Raio-X', 'Paracetamol 1000mg', '10488620031', '56782-ES');
 
 -- --------------------------------------------------------
 
@@ -84,11 +90,13 @@ CREATE TABLE `dose` (
 --
 
 INSERT INTO `dose` (`lote`, `numero_dose`, `data_aplicacao`, `nome`, `cpf`, `id_dose`) VALUES
-('022021', 1, '2021-10-05', 'AstraZeneca', '14921391491', 2),
-('042021', 2, '2021-10-20', 'AstraZeneca', '14921391491', 3),
-('072021', 1, '2021-10-17', 'Janssen', '10488620031', 4),
-('082021', 1, '2021-10-05', 'Pfizer', '58196919891', 5),
-('102021', 2, '2021-10-20', 'Pfizer', '58196919891', 6);
+('02/2021', 1, '2021-10-05', 'AstraZeneca', '14921391491', 2),
+('04/2021', 2, '2021-10-20', 'AstraZeneca', '14921391491', 3),
+('08/2021', 1, '2021-10-05', 'Pfizer', '58196919891', 5),
+('10/2021', 2, '2021-10-20', 'Pfizer', '58196919891', 6),
+('08/2021', 1, '2021-10-07', 'Janssen', '57726598638', 7),
+('08/2021', 1, '2021-10-07', 'Pfizer', '20446707120', 8),
+('10/2021', 2, '2021-11-17', 'Pfizer', '20446707120', 9);
 
 -- --------------------------------------------------------
 
@@ -125,6 +133,8 @@ CREATE TABLE `historico` (
 
 INSERT INTO `historico` (`cpf`, `nome`) VALUES
 ('14921391491', 'Clara Rayssa Olivia Ribeiro'),
+('20446707120', 'Elza Mariah da Cunha'),
+('57726598638', 'Pedro Ramos'),
 ('58196919891', 'Diego Hugo Breno Moraes');
 
 -- --------------------------------------------------------
@@ -143,6 +153,7 @@ CREATE TABLE `medico` (
 --
 
 INSERT INTO `medico` (`cpf`, `crm`) VALUES
+('24066389600', '32474-GO'),
 ('28609252604', '56782-ES');
 
 -- --------------------------------------------------------
@@ -168,7 +179,10 @@ CREATE TABLE `usuario` (
 INSERT INTO `usuario` (`nome`, `cpf`, `senha`, `email`, `data_nascimento`, `sexo`, `telefone`) VALUES
 ('Henry Lorenzo Vinicius Jesus', '10488620031', '8zkZoYB6ZG', 'henrylorenzoviniciusjesus@milimoveis.com.br', '2000-10-01', 'M', '68999865775'),
 ('Clara Rayssa Olivia Ribeiro', '14921391491', '01234', 'clararayssaoliviaribeiro_@grupoitamaraty.com.br', '1978-08-02', 'F', '71996740517'),
+('Elza Mariah da Cunha', '20446707120', 'Y0C2OpmUZs', 'elzamariahdacunha_@db9.com.br', '2000-05-17', 'F', '21989036259'),
+('Lorenzo Giovanni Fogaça', '24066389600', '1jZ3LCPE94', 'lorenzogiovannifogaca-95@csjsistemas.com.br', '1972-08-06', 'M', '92982462039'),
 ('Fátima Cecília Jéssica Silva', '28609252604', 'xHQaINqoeR', 'fatimaceciliajessicasilva__fatimaceciliajessicasilva@montcalm.com.br', '1957-11-25', 'F', '31995861584'),
+('Pedro Ramos', '57726598638', '43boHEOG84', 'pedroramos@gmail.com', '1988-11-07', 'M', '67992113436'),
 ('Diego Hugo Breno Moraes', '58196919891', '12345', 'diegohugobrenomoraes@folha.com.br', '1998-07-14', 'M', '28992513251');
 
 -- --------------------------------------------------------
@@ -207,7 +221,11 @@ CREATE TABLE `vinculo` (
 --
 
 INSERT INTO `vinculo` (`cpf`, `crm`) VALUES
+('14921391491', '32474-GO'),
 ('14921391491', '56782-ES'),
+('20446707120', '32474-GO'),
+('57726598638', '32474-GO'),
+('58196919891', '32474-GO'),
 ('58196919891', '56782-ES');
 
 --
@@ -283,13 +301,13 @@ ALTER TABLE `vinculo`
 -- AUTO_INCREMENT de tabela `consulta`
 --
 ALTER TABLE `consulta`
-  MODIFY `id_consulta` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_consulta` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT de tabela `dose`
 --
 ALTER TABLE `dose`
-  MODIFY `id_dose` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id_dose` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- Restrições para despejos de tabelas
